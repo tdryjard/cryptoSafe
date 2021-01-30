748bb31feb66c217c2ab345dec73a018079cdce5
Thomas.dryjard <newwbuiss@gmail.com>
2021-01-30 17:53:56 +0100

    deelte suspense index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -25,9 +25,7 @@ const root = document.getElementById("root");
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter basename={Config.basename}>
-      <Suspense fallback={Spinner}>
         <App />
-      </Suspense>
     </BrowserRouter>
   </Provider>,
   root
