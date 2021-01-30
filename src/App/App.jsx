aafa389ad69fceeda8ad69b6dc49ecb64def7363
Thomas.dryjard <newwbuiss@gmail.com>
2021-01-30 19:59:52 +0100

    test

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,12 +1,22 @@
 import React from "react";
 import Demo from '../containers/templateConfig/mainLayout';
 import { library } from "@fortawesome/fontawesome-svg-core";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Login from '../components/login/Login'
+import Landing from '../view/HomePage'
 
 import fontAwesomeIcon from "../utility/icons/fontawesome";
 library.add(fontAwesomeIcon);
 
 window.$ = window.jQuery = require('jquery')
 
-const App = () => <Demo />;
+function App() {
+    return (
+      <Switch>
+        <Route exact path="/" component={Landing} />
+        <Route path="/login" component={Login} />
+      </Switch>
+    );
+  }
 
 export default App;
