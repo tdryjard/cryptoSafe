f3dac570761acd48ac4cf98233c675cb703e5db3
Thomas.dryjard <newwbuiss@gmail.com>
2021-01-31 19:30:40 +0100

    test

--- src/containers/routes/mainRoutes.jsx
+++ src/containers/routes/mainRoutes.jsx
@@ -3,13 +3,11 @@ import {BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import routes from '../../routse'
 import Spinner from '../../components/spinner/Spinner';
 import HomePage from '../../view/HomePage'
-import Login from '../../components/login/Login'
 
 const Routes = props => (
   <Suspense fallback={Spinner}>
     <Switch>
       <Route exact path="/" component={HomePage} />
-      <Route path="/login" component={Login} />
     </Switch>
   </Suspense>
 )
